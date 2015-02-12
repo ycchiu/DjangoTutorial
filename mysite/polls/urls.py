b20c934c9ea50e5882ebae0950786089afb5840e
yachingc <tracy720329@gmail.com>
2015-02-11 22:19:59 -0800

    Begin, official tutorial first

---
+++ mysite/polls/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, url
+
+from polls import views
+
+urlpatterns = patterns('',
+	# ex: /polls/
+	url(r'^$', views.index, name='index'),
+
+	# ex: /polls/5/;  #the 'name' value as called by the {% url %} in template
+	url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+
+	#ex: /polls/5/result/
+	url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+
+	#ex: /polls/5/vote/
+	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+)
