5e7976e19a067cd74e611f6618e02a6233328155
yachingc <tracy720329@gmail.com>
2015-03-04 21:29:50 -0800

    final tutorial - test

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,10 +2,27 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from polls.models import Choice, Question
 
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
+	def get_queryset(self):
+		""" Return the last five published questions """
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
+'''
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	context = {'latest_question_list': latest_question_list}
@@ -18,6 +35,7 @@ def detail(request, question_id):
 def results(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/results.html', {'question':question})
+'''
 
 def vote(request, question_id):
 	p = get_object_or_404(Question, pk=question_id)
