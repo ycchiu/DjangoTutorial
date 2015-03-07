019a548830eaaf3dd8e9f7ae2fa86ff5fc0e60bc
yachingc <tracy720329@gmail.com>
2015-03-06 20:50:44 -0800

    first - make links model

---
+++ mysite/reddit/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Links',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title_text', models.CharField(max_length=100)),
+                ('url', models.CharField(max_length=1000)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- mysite/reddit/models.py
+++ mysite/reddit/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Links(models.Model):
+    title_text = models.CharField(max_length=100)
+    url = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return self.title_text + ":" + self.url;
+    
