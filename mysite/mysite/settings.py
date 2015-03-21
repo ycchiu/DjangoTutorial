6537fe9e17a69c80978b12d0ef2678943498c24d
yachingc <tracy720329@gmail.com>
2015-03-20 19:37:53 -0700

    progress - add css

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,7 +83,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+#STATICFILES_DIRS = (
+#    "/static/",
+#    "reddit/static/",
+#)
 
 LOGIN_REDIRECT_URL = '/'
--- mysite/reddit/urls.py
+++ mysite/reddit/urls.py
@@ -5,6 +5,6 @@ from reddit import views
 urlpatterns = patterns('',
     url(r'^$', views.link_list, name='index'),
     url(r'^reddit/new/$', views.link_new, name='link_new'),
-    url(r'^reddit/(?P<pk>[0-9+])/edit/$', views.link_edit, name='link_edit'),
-    url(r'^reddit/(?P<pk>[0-9+])/remove/$', views.link_delete, name='link_delete'),
+    url(r'^reddit/(?P<pk>[0-9]+)/edit/$', views.link_edit, name='link_edit'),
+    url(r'^reddit/(?P<pk>[0-9]+)/remove/$', views.link_delete, name='link_delete'),
 )
